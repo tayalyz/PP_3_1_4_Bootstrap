6c4b2668c94b0b8bf1efe2459ae8a762b96e0e02
tayalyz <107634713+tayalyz@users.noreply.github.com>
2023-01-13 18:01:49 +0300

    LAZY

--- src/main/java/ru/kata/spring/boot_security/demo/dao/UserDaoImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/dao/UserDaoImpl.java
@@ -19,19 +19,19 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public List<User> getAllUsers() {
-        return entityManager.createQuery("select u from User u", User.class).getResultList();
+        return entityManager.createQuery("select u from User u join fetch u.roles", User.class).getResultList();
     }
 
     @Override
     public User getUserById(Long id) {
-        TypedQuery<User> query = entityManager.createQuery("select u from User u where id = :id", User.class)
+        TypedQuery<User> query = entityManager.createQuery("select u from User u join fetch u.roles where u.id = :id", User.class)
                 .setParameter("id", id);
         return query.getSingleResult();
     }
 
     @Override
     public Optional<User> getUserByUsername(String username) {
-        TypedQuery<User> query = entityManager.createQuery("select u from User u where username = :username", User.class)
+        TypedQuery<User> query = entityManager.createQuery("select u from User u join fetch u.roles where u.username = :username", User.class)
                 .setParameter("username", username);
         return query.getResultList().stream().findFirst();
     }
